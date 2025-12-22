707a269025cb45cf391d2efc3948b58040a43196
Alanna Matieu <alannamatieu@MacBookPro.lan>
2025-12-21 21:48:37 -0500

    feat: add content to landing page and create footer

---
+++ dot-platform/src/features/landing/Footer.tsx
@@ -0,0 +1,28 @@
+import { Link } from 'react-router-dom';
+
+export const Footer = () => {
+  return (
+    <footer className="flex justify-between bg-red-600">
+      <div>
+        <ul>
+          <li>Products</li>
+          <li>Payments</li>
+          <li>Subscriptions</li>
+          <li>Connect</li>
+        </ul>
+        <ul>
+          <li>Developers</li>
+          <li>Payments</li>
+          <li>Subscriptions</li>
+          <li>Connect</li>
+        </ul>
+        <ul>
+          <li>Company</li>
+          <li>About</li>
+          <li>Blog</li>
+          <li>Contact</li>
+        </ul>
+      </div>
+    </footer>
+  );
+};
--- dot-platform/src/features/landing/LandingPage.tsx
+++ dot-platform/src/features/landing/LandingPage.tsx
@@ -9,17 +9,36 @@ Simple, clean engaging UI to include:
     - description
     - footer
 */
-import { Nav } from "./Nav";
+import { Nav } from './Nav';
+import { Footer } from './Footer';
 
 const LandingPage = () => {
   return (
     <div>
       <Nav />
       <section>
-        <h1>
-          Welcome to <span>Dot</span>!
-        </h1>
+        <h1>The new standard for online payments</h1>
+        <p>
+          Dot is the best software platform for running an internet business. We handle billions of
+          dollars every year for forward-thinking businesses around the world.
+        </p>
+        <button>Start now</button>
+        <button>Create an account</button>
       </section>
+      <section>list of businesses</section>
+      <section>
+        <h2>The complete toolkit for internet business</h2>
+        <p>
+          Dot builds the most powerful and flexible tools for internet commerce. Whether you’re
+          creating a subscription service, an on-demand marketplace, an e-commerce store, or a
+          crowdfunding platform, Dot's meticulously-designed APIs and unmatched functionality help
+          you create the best possible product for your users. Hundreds of thousands of the world’s
+          most innovative technology companies are scaling faster and more efficiently by building
+          their businesses on Dot.
+        </p>
+        <button>Discover how businesses use Dot</button>
+      </section>
+      <Footer />
     </div>
   );
 };
